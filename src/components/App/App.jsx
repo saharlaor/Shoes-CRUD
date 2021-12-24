81fcc5dcca060da54e4eaea6abf9e5c9606b54c6
saharoori <saharlaor@gmail.com>
2021-12-24 19:37:39 +0200

    App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,56 @@
 import React from "react";
-import "./App.css";
+import ShoeCard from "../ShoeCard/ShoeCard";
 
-function App() {
-  return <div className="App"></div>;
+import api from "../../api/api";
+
+import "./App.css";
+import Create from "../Create/Create";
+
+class App extends React.Component {
+  state = {
+    products: [],
+    createVis: true,
+  };
+
+  getProducts = async () => {
+    const { data } = await api.get();
+    this.setState({ products: data });
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  render() {
+    console.log(this.state.createVis);
+    const createObj = this.state.createVis ? (
+      <button
+        className="create-btn"
+        onClick={() => this.setState({ createVis: false })}>
+        Create a new shoe
+      </button>
+    ) : (
+      <Create
+        getProducts={this.getProducts}
+        hideCreate={() => this.setState({ createVis: true })}
+      />
+    );
+    const shoeList = this.state.products.map((product) => {
+      return (
+        <ShoeCard
+          getProducts={this.getProducts}
+          product={product}
+          key={product.id}
+        />
+      );
+    });
+    return (
+      <div className="App">
+        {createObj}
+        <div className="App__shoe-list">{shoeList}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
